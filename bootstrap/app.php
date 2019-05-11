49186844bd824033a913080880f70f3b9e120559
Zharkov Denis <zdu.ing@gmail.com>
2019-05-12 00:04:54 +0600

    set debug bar

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,6 +23,12 @@ $app = new Laravel\Lumen\Application(
 
 $app->withFacades();
 
+if (env('APP_DEBUG')) {
+$app->configure('app');
+$app->configure('debugbar');
+$app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
+}
+
 // $app->withEloquent();
 
 /*
